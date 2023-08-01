5b45b95f9f2bda00ffaa2b70293cb52947fb3529
aurélien <herbreteauaurelien@hotmail.fr>
2023-08-01 21:37:58 +0200

    hublogin

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
         {/* Conditional rendering based on userRole */}
         {userRole === '' && <NavbarDesktop />}
         {userRole === undefined && <NavbarDesktop />}
-        {userRole === 'player' && <NavbarDesktop />}
+        {userRole === 'client' && <NavbarDesktop />}
         {userRole === 'stringer' && <NavbarDesktopStringer />}
         {userRole === 'hub' && <NavbarDesktopHub />}
 
