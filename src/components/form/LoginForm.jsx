264ef73f3115fe15b9c5e696004062d9c60a401d
aurélien <herbreteauaurelien@hotmail.fr>
2023-08-31 08:31:33 +0200

    password reinitialise

--- src/components/form/LoginForm.jsx
+++ src/components/form/LoginForm.jsx
@@ -1,182 +1,195 @@
-import { useState } from "react"
-import {useForm} from "react-hook-form"
-import { yupResolver } from '@hookform/resolvers/yup'
-import { useStore } from "react-redux"
-import { Input, InputAdornment, IconButton, styled } from '@mui/material';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useStore } from "react-redux";
+import {
+  Input,
+  InputAdornment,
+  IconButton,
+  styled,
+  TextField,
+} from "@mui/material";
 
-import Visibility from '@mui/icons-material/Visibility'
-import VisibilityOff from '@mui/icons-material/VisibilityOff'
-import ModalResetPassword from '../modal/modalReset/ModalResetPassword'
-import { shemaInputLogin } from '../../Utils/shemaInput'
-import {connectedToggle, setUserInfo, setToken} from '../../store/userSlice'
-import {resetStringFromShopChoice} from '../../store/cartSlice'
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import ModalResetPassword from "../modal/modalReset/ModalResetPassword";
+import { shemaInputLogin } from "../../Utils/shemaInput";
+import {
+  connectedToggle,
+  setUserInfo,
+  setToken,
+} from "../../store/userSlice";
+import { resetStringFromShopChoice } from "../../store/cartSlice";
 
-export default function LoginForm( props ) {
-
- // gestion du style de l'input mui material
- const CustomInput = styled(Input)(({ theme }) => ({
-  appearance: 'none !important',
-  display: 'flex',
-  fontSize: '1.5rem',
-  fontWeight: 'bolder',
-  backgroundColor: '#e7e4e4 !important',
-  border: 'none !important',
-  minHeight: '4.4rem',
-  padding: '0 1.6rem',
-  borderRadius: '1rem !important',
-  maxWidth: '40rem',
-  whiteSpace: 'normal',
-  overflow: 'hidden',
-  wordWrap: 'break-word',
-  overflowWrap: 'break-word',
-  textOverflow: 'ellipsis',
-  outline: 'none', // Pour supprimer le contour de focus par défaut
-  '&:before': {
-    content: 'none',
-  },
-  '&:focus': {
-    outline: 'none', // Pour supprimer le contour de focus par défaut
-    boxShadow: 'none', // Pour supprimer l'ombre de focus par défaut
-    '&:after': {
-      content: 'none',
-    },
-  }, 
+const CustomTextField = styled(TextField)(({ theme }) => ({
+  // Votre style personnalisé pour le champ de texte
 }));
 
-  // gestion de l'affichage de l'erreur backend dans la balise p
-  const [isPErrorFromBackEndOpen, setShowErrorFromBackEnd] = useState(false) ;
-  const showPErrorFromBackend = () => {
-   setShowErrorFromBackEnd( true );
- };
+const CustomInput = styled(Input)(({ theme }) => ({
+  appearance: "none !important",
+  display: "flex",
+  fontSize: "1.5rem",
+  fontWeight: "bolder",
+  backgroundColor: "#e7e4e4 !important",
+  border: "none !important",
+  minHeight: "4.4rem",
+  padding: "0 1.6rem",
+  borderRadius: "1rem !important",
+  maxWidth: "40rem",
+  whiteSpace: "normal",
+  overflow: "hidden",
+  wordWrap: "break-word",
+  overflowWrap: "break-word",
+  textOverflow: "ellipsis",
+  outline: "none",
+  "&:before": {
+    content: "none",
+  },
+  "&:focus": {
+    outline: "none",
+    boxShadow: "none",
+    "&:after": {
+      content: "none",
+    },
+  },
+}));
 
-  // gestion de l'affichage du mot de passe
-  const [showPassword, setShowPassWord] = useState(false) ;
+export default function LoginForm(props) {
+  const [isPErrorFromBackEndOpen, setShowErrorFromBackEnd] = useState(false);
+  const showPErrorFromBackend = () => {
+    setShowErrorFromBackEnd(true);
+  };
+
+  const [showPassword, setShowPassWord] = useState(false);
   const handleClickShowPassword = () => {
-    setShowPassWord( !showPassword );
+    setShowPassWord(!showPassword);
   };
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
 
-  // gestion du contôle de la validité des inputs 
-  const { register , formState, handleSubmit,   formState: { errors }  } =
-   useForm({
+  const {
+    register,
+    formState,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(shemaInputLogin),
-    mode: 'onTouched',
+    mode: "onTouched",
     shouldFocusError: true,
   });
 
-  const { isValid} = formState;
+  const { isValid } = formState;
 
-   // gestion de la valeure de la réponse backend 
-   const [messageFromBackEnd, setMessageFromBackend] = useState("") ;
-   function changeMessageFromBackEnd(messageFromBack) {
-     setMessageFromBackend(messageFromBack);
-   }
+  const [messageFromBackEnd, setMessageFromBackend] = useState("");
+  function changeMessageFromBackEnd(messageFromBack) {
+    setMessageFromBackend(messageFromBack);
+  }
 
-  const store = useStore()
+  const store = useStore();
 
-
-  // gestion de l'ouverture du modale de réinitialisation du mot de passe
-  const [isModalResetPasswordOpen, setModalResetPasswordOpen] = useState(false) ;
+  const [isModalResetPasswordOpen, setModalResetPasswordOpen] = useState(false);
   const showModalResetPassword = () => {
     setModalResetPasswordOpen(true);
   };
-  const closeModalResetPassword = function(){
+  const closeModalResetPassword = function () {
     setModalResetPasswordOpen(false);
   };
 
-
-  const closeModalConnexion = props.closeModalConnexion
-
-  //fonction asynchrone vers le backend verifiant l'email et le mot de passe associé
-  //dans un second temps on recupère les informations utilisateurs et on les enregistre dans le store
+  const closeModalConnexion = props.closeModalConnexion;
 
   const onSubmit = async function (data) {
+    try {
+      const response = await fetch(
+        "https://click-backend.herokuapp.com/api/user/login",
+        {
+          mode: "cors",
+          method: "POST",
+          body: JSON.stringify({ email: data.email, password: data.password }),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
 
-    try{
-      const response = await fetch(`https://click-backend.herokuapp.com/api/user/login`, {
-        mode: "cors",
-        method: "POST",
-        body: JSON.stringify({email: data.email, password: data.password}),
-        headers: {"Content-Type": "application/json"}})
-
-        if (!response.ok) {
-          const result = await response.json();
-          changeMessageFromBackEnd(result.message);
-          showPErrorFromBackend();
-          throw new Error(` ${result.message}`);
-        }else {
+      if (!response.ok) {
+        const result = await response.json();
+        changeMessageFromBackEnd(result.message);
+        showPErrorFromBackend();
+        throw new Error(` ${result.message}`);
+      } else {
         const result = await response.json();
         store.dispatch(setUserInfo(result.userInfo));
-        console.log("userInfo", result.userInfo)
+        console.log("userInfo", result.userInfo);
         store.dispatch(connectedToggle());
-        store.dispatch(setToken(result.token))
-        store.dispatch( resetStringFromShopChoice(result.userInfo.stringInfo))
-        closeModalConnexion(); 
+        store.dispatch(setToken(result.token));
+        store.dispatch(resetStringFromShopChoice(result.userInfo.stringInfo));
+        closeModalConnexion();
       }
-    }
-
-    catch(err){
+    } catch (err) {
       const errorMessage = err.toString();
       console.log(errorMessage);
     }
   }
 
-
   return (
-
     <form onSubmit={handleSubmit(onSubmit)} className="form-modal">
-
-      <label className="input__label"> Email <sup>*</sup>  </label>
-      <input type="email" {...register("email")} className="input__text" />
+      <label className="input__label"> Email </label>
+      <input
+        type="email"
+        {...register("email")}
+        className="input__text"
+      />
       <p className="input__error">{errors.email?.message}</p>
 
-      <label className="input__label"> Mot de passe <sup>*</sup> </label>
-      
+      <label className="input__label"> Mot de passe </label>
+
       <CustomInput
-        type={showPassword ? 'text' : 'password'}
-        {...register('password')}
+        type={showPassword ? "text" : "password"}
+        {...register("password")}
         endAdornment={
           <InputAdornment position="end">
-            <IconButton onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword}>
-              {showPassword ? <Visibility className="eye" /> : <VisibilityOff className="eye" />}
+            <IconButton
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+            >
+              {showPassword ? (
+                <Visibility className="eye" />
+              ) : (
+                <VisibilityOff className="eye" />
+              )}
             </IconButton>
           </InputAdornment>
         }
       />
 
-        <div className="reset-password-link" onClick={showModalResetPassword}>
-          Mot de passe oublié ?
-        </div>
+      <div className="reset-password-link" onClick={showModalResetPassword}>
+        Mot de passe oublié ?
+      </div>
 
-      {isModalResetPasswordOpen ?  
+      {isModalResetPasswordOpen ? (
         <ModalResetPassword
-        closeModalResetPassword = {closeModalResetPassword}
-        closeModalConnexion = {closeModalConnexion}
+          closeModalResetPassword={closeModalResetPassword}
+          closeModalConnexion={closeModalConnexion}
         />
-        :""}
+      ) : (
+        ""
+      )}
 
       <p className="input__error">{errors.password?.message}</p>
-      {isPErrorFromBackEndOpen ?  
+      {isPErrorFromBackEndOpen ? (
         <p className="input__error message__error">{messageFromBackEnd}</p>
-        :""}
-
+      ) : (
+        ""
+      )}
 
       <div className="submit-wrapper-connexion">
-
-        <button 
-        disabled={ !isValid} 
-        type="submit" 
-        className={isValid ? "btn btn-blue" : "btn btn-blue-invalid"}  
+        <button
+          disabled={!isValid}
+          type="submit"
+          className={isValid ? "btn btn-blue" : "btn btn-blue-invalid"}
         >
           connexion
         </button>
-
-      </div> 
-
+      </div>
     </form>
-
-
-  )
+  );
 }
